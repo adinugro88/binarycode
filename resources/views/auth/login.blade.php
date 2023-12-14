5aae0c78311b22170be35f9e6baa01f2a0b9864c
adinugro88 <adin5337@gmail.com>
2023-12-14 21:27:05 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -66,6 +66,64 @@
                         </div>
                     </form>
                 </div>
+            </div>
+        </div>
+    </div>
+</div> --}}
+
+
+<div class="row justify-content-md-center">
+    <div class="col-md-12 col-lg-4">
+        <div class="card login-box-container">
+            <div class="card-body">
+                <div class="authent-logo">
+                    <img src="../../assets/images/logo@2x.png" alt="">
+                </div>
+                <div class="authent-text">
+                    <p>Welcome to Circl</p>
+                    <p>Please Sign-in to your account.</p>
+                </div>
+
+                <form method="POST" action="{{ route('login') }}">
+                       @csrf
+                    <div class="mb-3">
+                        <div class="form-floating">
+                            <input type="email" id="floatingInput" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <label for="floatingInput">Email address</label>
+
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <div class="form-floating">
+                            <input type="password"  id="floatingPassword" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                            <label for="floatingPassword">Password</label>
+                            @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
+                          </div>
+                    </div>
+                
+                    <div class="d-grid">
+                    <button type="submit" class="btn btn-info m-b-xs">Sign In</button>
+                    
+
+                    @if (Route::has('password.request'))
+                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                        {{ __('Forgot Your Password?') }}
+                    </a>
+                @endif
+                </div>
+                  </form>
+                  <div class="authent-reg">
+                      <p>Not registered? <a href="register.html">Create an account</a></p>
+                  </div>
             </div>
         </div>
     </div>
