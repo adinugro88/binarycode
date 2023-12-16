e599105c21357c2c620ea6558eef2381e9fd1355
adinugro88 <adin5337@gmail.com>
2023-12-16 23:47:05 +0700

    update route

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,26 @@
+<div class="page-sidebar">
+    <ul class="list-unstyled accordion-menu">
+        <li class="sidebar-title">
+            Main
+        </li>
+        <li>
+            <a href="index.html"><i data-feather="home"></i>Dashboard</a>
+        </li>
+        <li class="sidebar-title">
+            Apps
+        </li>
+        <li>
+            <a href="social.html"><i data-feather="user"></i>Sertifikat</a>
+        </li>
+      
+        <li class="sidebar-title">
+            Settings
+        </li>
+        <li>
+            <a href="/student"><i data-feather="user"></i>Students</a>
+        </li>
+        <li>
+            <a href=""><i data-feather="calendar"></i>Kategori</a>
+        </li>
+    </ul>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
