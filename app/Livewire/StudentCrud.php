1e0f06cd3d6b8fd39a9aabcd24d6adfb62ca029d
adinugro88 <adin5337@gmail.com>
2023-12-24 21:50:51 +0700

    update new '

--- app/Livewire/StudentCrud.php
+++ app/Livewire/StudentCrud.php
@@ -10,9 +10,27 @@ class StudentCrud extends Component
 {
     use WithPagination;
     public $no = 1;
+    public $nonik,$name,$kelas,$sekolah,$alamat,$phone;
+
     public function render()
     {
         $student = Student::latest()->paginate(10);
         return view('livewire.student-crud', ['student' => $student]);
+    }
+
+
+    public function reset()
+    {
+        $nonik      = "";
+        $name       = "";
+        $kelas      = "";
+        $sekolah    = "";
+        $alamat     = "";
+        $phone      = "";
+    }
+
+    public function tambah ()
+    {
+        
     }
 }
