4a3aefd679eb4a0a42eb8491f33d7dd8587e7710
adinugro88 <adin5337@gmail.com>
2024-02-05 12:59:42 +0700

    update

--- resources/views/pages/trialcardpdf.blade.php
+++ resources/views/pages/trialcardpdf.blade.php
@@ -4,62 +4,65 @@
 <style>
     body {
         /* A5 dimensions */
-        height: 170mm;
+        height: 155mm;
         width: 210mm;
-        border: 5px solid #003100;
+        border: 10px solid #278fcd ;
         margin: 0;
     }
 
-
+    .tulisan{
+        color: #89894c;
+    }
 </style>
 @endsection
 
 @section('content')
 
-<div class="container mx-auto">
-    <div class="row">
-        <div class="row birumuda">
-            <div class="col-md-6">
-                <h3 style="font-size: 40px;vertical-align:baseline"><b> TRIAL CARD</b></h3>
+<div class="container mx-auto p-0">
+        <div class="row birumuda" style="padding:0 12px">
+            <div class="col-md-4  py-1" style="background-color: beige">
+               
+                <img src="{{asset('logo.png')}}" width="95" alt="">
             </div>
-            <div class="col-md-6 text-right" style="text-align:right;">
-                <b style="font-size: 20px;">{{ Carbon\Carbon::parse($card->tanggal)->format('d F Y')  }}</b>
+            <div class="col-md-4 text-center" style="background-color: beige">
+                <h3 style="font-size: 40px;vertical-align:baseline"><b class="tulisan"> TRIAL CARD</b></h3>
+            </div>
+            <div class="col-md-4 text-right" style="text-align:right;background-color: beige">
+                <p class="tulisan" style="font-size: 20px;margin:10px 0;font-weight: 700"> {{ Carbon\Carbon::parse($card->tanggal)->format('d F Y')  }}</p>
             </div>
         </div>
-        <hr style=" border: 8px solid rgb(0, 49, 0);opacity:1">
+        <hr style=" border: 8px solid #278fcd ;opacity:1;margin:0">
         {{-- <div class="col-12 mb-2 text-end" style="font-size: 25px"> --}}
-            
-        <div class="row">
-            <div class="col-4">
-                <b style="font-size: 20px">STUDENT NAME</b>
+        <div class="row" style="padding:0 12px;">
+            <div class="col-4" style="background-color: beige;">
+                <b class="tulisan" style="font-size: 20px">STUDENT NAME</b>
             </div>
-            <div class="col-8">
-                <span style="font-size: 20px" class="text-uppercase">: {{$card->Student->name}}</span>
+            <div class="col-8" style="background-color: beige;">
+                <b class="tulisan"  style="font-size: 20px" class="text-uppercase">: {{$card->Student->name}}</b>
             </div>
-            <div class="col-4">
-                <b style="font-size: 20px">CLASS</b>
+            <div class="col-4" style="background-color: beige;">
+                <b class="tulisan" style="font-size: 20px">CLASS</b>
             </div>
-            <div class="col-8">
-                <span style="font-size: 20px" class="text-uppercase">: {{$card->Kategori->nama}}</span>
+            <div class="col-8" style="background-color: beige;">
+                <b class="tulisan" style="font-size: 20px" class="text-uppercase">: {{$card->Kategori->nama}}</b>
             </div>
-            <div class="col-4">
-                <b style="font-size: 20px">CONTACT</b>
+            <div class="col-4" style="background-color: beige;">
+                <b class="tulisan" style="font-size: 20px">CONTACT</b>
             </div>
-            <div class="col-8">
-                <span style="font-size: 20px"
-                    class="text-uppercase">: {{$card->Student->phone}}</span>
-
+            <div class="col-8" style="background-color: beige;">
+                <b class="tulisan" style="font-size: 20px"
+                    class="text-uppercase">: {{$card->Student->phone}}</b>
             </div>
         </div>
 
-        <table class="table table-striped mt-3">
+        <table class="table table-striped mt-0">
             <thead class="thead-dark">
                 <tr>
-                    <th style="text-align:center!important;font-size: 20px;background-color: rgb(0, 49, 0);color:white">
+                    <th style="font-size: 20px;background-color: #278fcd ;color:white">
                         SUBJECT</th>
-                    <th style="text-align:center;font-size: 20px;background-color: rgb(0, 49, 0);color:white">
+                    <th style="text-align:center;font-size: 20px;background-color: #278fcd ;color:white">
                         SCORE</th>
-                    <th style="text-align:center;font-size: 20px;background-color: rgb(0, 49, 0);color:white">
+                    <th style="text-align:center;font-size: 20px;background-color: #278fcd ;color:white">
                         DETAIL</th>
                 </tr>
             </thead>
@@ -72,11 +75,11 @@
             @endforeach
 
         </table>
-        <table class="table table-striped mt-3">
+        <table class="table table-striped mt-0">
             <thead class="thead-dark">
                 <tr>
                     <th
-                        style="text-align:center!important;font-size: 20px;background-color: #003100;color:white">
+                        style="text-align:center!important;font-size: 20px;background-color: #278fcd ;color:white">
                         NOTES</th>
                 </tr>
             </thead>
@@ -90,9 +93,6 @@
                 <p class="" style="font-size: 20px">www.binarykiddo.id</p>
             </div>
         </div> --}}
-
-
-    </div>
 </div>
 
 
