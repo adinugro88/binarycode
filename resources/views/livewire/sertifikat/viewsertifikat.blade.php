610ec89c8d11bf1b9fb1a57d31ccfcab7bddce36
adinugro88 <adin5337@gmail.com>
2024-02-08 22:31:22 +0700

    update

--- resources/views/livewire/sertifikat/viewsertifikat.blade.php
+++ resources/views/livewire/sertifikat/viewsertifikat.blade.php
@@ -2,8 +2,8 @@
     <div class="col">
         <div class="card">
             <div class="card-body">
-              
-            
+
+
                 <div class="row">
                     <div class="col-md-6">
                         <h3 style="font-size: 60px"><b>STUDENT REPORT</b> </h3>
@@ -16,30 +16,30 @@
                 <div class="col-12 mb-2" style="font-size: 35px" ><b>Binary Kiddo</b> </div>
                 <div class="row">
                     <div class="col-2">
-                       <b style="font-size: 20px">STUDENT NAME :</b> 
+                       <b style="font-size: 20px">STUDENT NAME :</b>
                     </div>
                     <div class="col-10">
                         <span style="font-size: 20px" class="text-uppercase">{{$card->Student->name}}</span>
-                        
+
                     </div>
                     <div class="col-2">
-                        <b style="font-size: 20px">STUDENT ID NO :</b> 
+                        <b style="font-size: 20px">STUDENT ID NO :</b>
                     </div>
                     <div class="col-10">
-                        <span style="font-size: 20px">{{$card->Student->nonik}}</span> 
+                        <span style="font-size: 20px">{{$card->Student->nonik}}</span>
                     </div>
                     <div class="col-2">
-                        <b style="font-size: 20px">CLASS :</b> 
+                        <b style="font-size: 20px">CLASS :</b>
                     </div>
                     <div class="col-10">
-                        <span style="font-size: 20px" class="text-uppercase">{{$toclass}} asdasd</span> 
+                        <span style="font-size: 20px" class="text-uppercase">{{$toclass}} asdasd</span>
                     </div>
                     <div class="col-2">
-                        <b style="font-size: 20px">TERM :</b> 
+                        <b style="font-size: 20px">TERM :</b>
                     </div>
                     <div class="col-10">
                         <span style="font-size: 20px" class="text-uppercase">{{ Carbon\Carbon::parse($card->tanggal)->format('F Y')  }}</span>
-                        
+
                     </div>
                 </div>
 
@@ -56,7 +56,7 @@
                         <td style="text-align:center;font-size: 20px">{{$item['score']}}</td>
                     </tr>
                     @endforeach
-                  
+
                 </table>
 
                 <div class="row">
@@ -65,7 +65,7 @@
                             <thead class="thead-dark">
                             <tr>
                                 <th  style="text-align:center!important;font-size: 20px;background-color: rgb(0, 49, 0);color:white">SCORE SCALE</th>
-                                
+
                             </tr>
                             </thead>
                             <tr>
@@ -84,23 +84,23 @@
                             <thead class="thead-dark">
                             <tr>
                                 <th  style="text-align:center!important;font-size: 20px;background-color: rgb(0, 49, 0);color:white">NOTES</th>
-                                
+
                             </tr>
                             </thead>
                             <tr>
                                 <td rowspan="3" style="font-size: 20px">{{$card->Note}}</td>
                             </tr>
-                           
+
                         </table>
 
                     </div>
-                    
+
                    <div class="col-md-12 mt-4 text-right" style="text-align: right">
                      <p class="" style="font-size: 20px">www.binarykiddo.id</p>
                    </div>
                 </div>
 
-                <a href="/admin/sertifikat" wire:click='setting' type="submit" class="btn btn-secondary">Back</a>
+                <a  href="/admin/sertifikat" wire:click='setting' type="submit" class="btn btn-secondary">Back</a>
                 <button class="btn btn-primary text-right">Download File PDF</button>
             </div>
             <div>
--- resources/views/pages/trialcard.blade.php
+++ resources/views/pages/trialcard.blade.php
@@ -6,8 +6,8 @@
     <div class="col">
         <div class="card">
             <div class="card-body">
-              
-            
+
+
                 <div class="row">
                     <div class="col-md-6">
                         <h3 style="font-size: 40px;vertical-align:baseline"><b> TRIAL CARD</b></h3>
@@ -20,23 +20,23 @@
                 <div class="col-12 mb-2 text-end" style="font-size: 25px" ><b>{{ Carbon\Carbon::parse($card->tanggal)->format('d F Y')  }}</b> </div>
                 <div class="row">
                     <div class="col-2">
-                       <b style="font-size: 20px">STUDENT NAME :</b> 
+                       <b style="font-size: 20px">STUDENT NAME :</b>
                     </div>
                     <div class="col-10">
                         <span style="font-size: 20px" class="text-uppercase">{{$card->Student->name}}</span>
                     </div>
                     <div class="col-2">
-                        <b style="font-size: 20px">CLASS :</b> 
+                        <b style="font-size: 20px">CLASS :</b>
                     </div>
                     <div class="col-10">
-                        <span style="font-size: 20px" class="text-uppercase">{{$toclass->nama}}</span> 
+                        <span style="font-size: 20px" class="text-uppercase">{{$toclass->nama}}</span>
                     </div>
                     <div class="col-2">
-                        <b style="font-size: 20px">TERM :</b> 
+                        <b style="font-size: 20px">TERM :</b>
                     </div>
                     <div class="col-10">
                         <span style="font-size: 20px" class="text-uppercase">{{ Carbon\Carbon::parse($card->tanggal)->format('d F Y')  }}</span>
-                        
+
                     </div>
                 </div>
 
@@ -55,7 +55,7 @@
                         <td style="text-align:center;font-size: 20px">{{$item['detail']}}</td>
                     </tr>
                     @endforeach
-                  
+
                 </table>
 
                 <div class="row">
@@ -64,7 +64,7 @@
                             <thead class="thead-dark">
                             <tr>
                                 <th  style="text-align:center!important;font-size: 20px;background-color: rgb(0, 49, 0);color:white">SCORE SCALE</th>
-                                
+
                             </tr>
                             </thead>
                             <tr>
@@ -83,24 +83,24 @@
                             <thead class="thead-dark">
                             <tr>
                                 <th  style="text-align:center!important;font-size: 20px;background-color: rgb(0, 49, 0);color:white">NOTES</th>
-                                
+
                             </tr>
                             </thead>
                             <tr>
                                 <td rowspan="3" style="font-size: 20px">{{$card->Note}}</td>
                             </tr>
-                           
+
                         </table>
 
                     </div>
-                    
+
                    <div class="col-md-12 mt-4 text-right" style="text-align: right">
                      <p class="" style="font-size: 20px">www.binarykiddo.id</p>
                    </div>
                 </div>
 
                 <a href="/admin/sertifikat"  type="submit" class="btn btn-secondary">Back</a>
-                <a href="/admin/sertifikat/download/{{$card->id}}" class="btn btn-primary text-right">Download File PDF</a>
+                <a  target="_blank" href="/admin/sertifikat/download/{{$card->id}}" class="btn btn-primary text-right">Download File PDF</a>
             </div>
             <div>
             </div>
